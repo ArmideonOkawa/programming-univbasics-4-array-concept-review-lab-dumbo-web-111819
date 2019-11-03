1bb45f3de467bdaf374046ad2bfaae90b54df63d
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-11-03 00:16:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,10 +13,18 @@ def find_max_value(array)
   array.length.times do |index|
     if array[index] > x 
       x = array[index]
-end
+  end
+end 
     x 
   end
 
 def find_min_value(array)
-
+    x = array[0]
+  array.length.times do |index|
+    if array[index] < x 
+      x = array[index]
+  end
+end 
+    x 
+  end
 end
